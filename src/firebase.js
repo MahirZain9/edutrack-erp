cdc465569466a72cd0ec663e3f2bc0814254e7f2
MahirZain9 <lifesurgetraining@gmail.com>
2026-06-16 16:44:39 +0530

    Add files via upload

--- src/firebase.js
+++ src/firebase.js
@@ -1,10 +1,9 @@
-
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBlHqQ84f-1kkrEzqipUr43YRVj91ZbKzY",
+  apiKey: "AIzaSyBlRqQ84f-IkkrEZqipUr43YRVj9IZ6KzY",
   authDomain: "edutrack-d4c4d.firebaseapp.com",
   projectId: "edutrack-d4c4d",
   storageBucket: "edutrack-d4c4d.firebasestorage.app",
@@ -16,7 +15,6 @@ const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
 const auth = getAuth(app);
 
-// Ye rahi wo missing line jo Vercel mang raha tha
 export const isFirebaseConfigured = true;
 
 export { db, auth };
