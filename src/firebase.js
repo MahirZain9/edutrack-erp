1a7d83e3133ce28b7560e99d2fb05c6329d16b00
MahirZain9 <lifesurgetraining@gmail.com>
2026-06-17 11:33:26 +0530

    Add files via upload

--- src/firebase.js
+++ src/firebase.js
@@ -3,12 +3,12 @@ import { getFirestore } from "firebase/firestore";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBlRqQ84f-IkkrEZqipUr43YRVj9IZ6KzY",
+  apiKey: "AIzaSyBlRqQ84F-IkkrEZqipUr43YRVj9IZ5KzY",
   authDomain: "edutrack-d4c4d.firebaseapp.com",
   projectId: "edutrack-d4c4d",
   storageBucket: "edutrack-d4c4d.firebasestorage.app",
   messagingSenderId: "1080893982796",
-  appId: "1:1080893982796:web:c3db8f15383aef6f438ce9"
+  appId: "1:1080893982796:web:c3db8f15383eef6f438ce9"
 };
 
 const app = initializeApp(firebaseConfig);
